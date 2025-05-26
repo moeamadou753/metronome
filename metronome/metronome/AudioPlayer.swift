34cc6c7721234f2da4eb024ff6eea6ba67bd4555
moe 🤘😊 <mohamed.amadou159@gmail.com>
2025-05-26 16:39:00 -0400

    add audio player and quartz metronome sound

---
+++ metronome/metronome/AudioPlayer.swift
@@ -0,0 +1,39 @@
+//
+//  AudioPlayer.swift
+//  metronome
+//
+//  Created by Moe Amadou on 5/26/25.
+//
+
+import Foundation
+import AVFoundation
+
+let DEFAULT_METRONOME_PATH: String = "audio/clicks/quartz-metronome"
+let DEFAULT_METRONOME_TYPE: String = ".wav"
+
+func resolveSound(relativePath: String, audioType: String) -> String? {
+    return Bundle.main.path(forResource: relativePath, ofType: audioType)
+}
+
+
+class AudioPlayer: ObservableObject {
+    var player: AVAudioPlayer?
+    @Published var isPlaying: Bool
+    
+    init() {
+        if let sound = resolveSound(relativePath: DEFAULT_METRONOME_PATH, audioType: DEFAULT_METRONOME_TYPE) {
+            do {
+                player = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: sound))
+            } catch {
+                print("Error processing audio path / type.")
+            }
+        } else {
+            print("Invalid audio path / type received.")
+        }
+        isPlaying = false
+    }
+    
+    func toggleAudioPlayer() {
+        
+    }
+}
