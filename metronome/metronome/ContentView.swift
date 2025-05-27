9116bafdaea2adaef64f01171009fcc046bc543c
moe 🤘😊 <mohamed.amadou159@gmail.com>
2025-05-26 20:03:38 -0400

    add slider, reset invalid text inputs

--- metronome/metronome/ContentView.swift
+++ metronome/metronome/ContentView.swift
@@ -11,20 +11,33 @@ import SwiftData
 struct ContentView: View {
     @StateObject var clock: Clock = Clock()
     @State var bpmInput: String = ""
-
+    @State var sliderValue: Double = 0
+    
     init() {
-        _bpmInput = State(initialValue: String($clock.bpm.wrappedValue))
+        _bpmInput = State(initialValue: String(clock.bpm))
+        _sliderValue = State(initialValue: Double(clock.bpm))
     }
     
-    func validate(bpm: String) {
+    func validate(bpm: String) -> Bool {
         let intValue = Int(bpm)
         
         if (intValue == nil) {
-            return
+            return false
         }
         
         if (intValue! > 0 && intValue! < 500) {
             clock.updateTimeSignature(bpm: intValue!)
+            return true
+        }
+        
+        return false
+    }
+    
+    func validate(bpm: Double) {
+        let intValue = Int(bpm)
+        
+        if (intValue > 0 && intValue < 500) {
+            clock.updateTimeSignature(bpm: intValue)
         }
     }
     
@@ -34,12 +47,21 @@ struct ContentView: View {
                 Section(header: Text("BPM")) {
                     TextField("", text: $bpmInput)
                         .onSubmit {
-                            validate(bpm: bpmInput)
+                            if (!validate(bpm: bpmInput)) {
+                                bpmInput = String(clock.bpm)
+                            }
                         }.disableAutocorrection(true)
                         .onChange(of: $clock.bpm.wrappedValue) {
                             bpmInput = String(clock.bpm)
                         }
                 }
+            }
+            Slider(value: $sliderValue,
+                   in: 1...300, step: 1,
+                   onEditingChanged: { isEditing in if (!isEditing) { validate(bpm: sliderValue)}}
+                   
+            ).onChange(of: $clock.bpm.wrappedValue) {
+                value in sliderValue = Double(value)
             }
             Button(action: $clock.running.wrappedValue ? clock.stop : clock.start) {
                 Text(">")
